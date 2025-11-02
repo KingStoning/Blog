3e0e3d216712a1396397027152251a1940e84a83
KingStoning <157497163+KingStoning@users.noreply.github.com>
2025-11-02 09:58:46 +0800

    修改

--- app/components/widget/BlogLog.vue
+++ app/components/widget/BlogLog.vue
@@ -3,11 +3,9 @@ const appConfig = useAppConfig()
 const timeEstablished = appConfig.timeEstablished
 
 const blogLog = [
-	{ label: '2025-07-26', value: '重构到 Nuxt 4 + Nuxt Content v3，启用 zhilu.site 域名' },
-	{ label: '2024-08-11', value: '重构到 Nuxt 3 + Nuxt Content v2' },
-	{ label: '2023-05-24', value: '更换框架为 Hexo，升级博客' },
-	{ label: '2020-08-24', value: '使用 zhilu.cyou 域名' },
-	{ label: timeEstablished, value: '发布第一篇文章' },
+	{ label: '2025-10-26', value: '开启新的博客框架' },
+	{ label: '2025-10-03', value: '数据库损坏,博客丢失' },
+	{ label: '2024-5-4', value: '接触到博客' },
 ]
 </script>
 
